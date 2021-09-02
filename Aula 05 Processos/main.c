4977c36403f0d3a87e04afdbb88d4640588e34f5
murilo-toddy <murilo.todao@usp.br>
2021-09-02 15:50:09 -0300

    Criação de programa Aula 05

--- Aula 05 Processos/main.c
+++ Aula 05 Processos/main.c
@@ -7,10 +7,12 @@ int main(void) {
     pid_t p;
 
     printf("creating process\n");
-    if((p = fork()) == 0) { //Cria um novo processo
+    if((p = fork()) == 0) { //Cria um novo processo (duplica o programa na memória)
         printf("child process\n");
+        execl("/usr/bin/ls", "ls", "-l", NULL);
     } else {
         printf("main process with child PID %d\n", p);
+        sleep(1);
     }
 
     return 0;
