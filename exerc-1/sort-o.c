10c208c8999feef764cfeae31ab023bf41fd00c0
murilo-toddy <murilo.todao@usp.br>
2021-10-14 15:14:42 -0300

    Finalização exercício 01

--- exerc-1/sort-o.c
+++ exerc-1/sort-o.c
@@ -3,12 +3,14 @@
 
 typedef struct node node_t;
 
+// Estrutura para armazenar informações de um nó específico
 struct node {
     int key;
     char *data;
     node_t *next, *prev;
 };
 
+// Estrutura para armazenar todos os nós (equivale ao item_t)
 typedef struct {
     node_t *head, *last;
     int size;
@@ -16,6 +18,7 @@ typedef struct {
 
 // Construtor
 list_t *list_create() {
+    // Alocação e definição dos parâmetros de uma lista vazia
     list_t *list = (list_t*)malloc(sizeof(list_t));
     list->size = 0;
     list->head = NULL;
@@ -24,6 +27,7 @@ list_t *list_create() {
 
 // Destrutor
 void list_destroy(list_t *list) {
+    // Desalocação dos nós e, em seguida, da lista
     node_t *node = list->head;
     while(node != NULL) {
         list->head = node->next;
@@ -33,21 +37,27 @@ void list_destroy(list_t *list) {
     free(list);
 }
 
+// Função responsável por alocar um novo nó na lista
 void list_insert(list_t *list, int key, char *data) {
     node_t *node = (node_t*)malloc(sizeof(node_t));
+    // Caso list->head == NULL, a lista está vazia
     if(list->head == NULL) { 
         list->head = node;
         node->prev = NULL;
     }
+    // Caso contrário, a lista possui pelo menos um elemento
     else { 
         list->last->next = node; 
         node->prev = list->last;
     }
+
+    // Definição dos parâmetros do nó
     list->last = node;
     list->size++;
     node->next = NULL;
     node->key = key;
 
+    // Alocação da string para um vetor com o tamanho desejado
     int string_size = 0;
     while(data[string_size++] != '\0');
     char *data_string = (char*)malloc(string_size * sizeof(char));
@@ -55,34 +65,45 @@ void list_insert(list_t *list, int key, char *data) {
     node->data = data_string;    
 }
 
+// Função para realizar a ordenação da lista com base nas chaves
 void list_sort(list_t *list) {
+    // Caso o tamanho seja menor ou igual a 1, não é necessário fazer nada
     if(list->size <= 1) { return; }
     int s = 1;
+
     for(int i = 0; i < list->size && s != 0; i++) {
         s = 0;
         node_t *node = list->head;
         while(node != NULL) {
+            // Verifica se as chaves estão fora de ordem
             if(node->next != NULL && node->key > node->next->key) {
                 node_t *next = node->next;
 
+                //Elemento é o primeiro da lista, deve ser alterado para elemento seguinte
                 if(list->head == node) { list->head = next; }
+                
+                // Elemento seguinte é o último da lista, deve ser alterado para elemento
                 if(list->last == next) { list->last = node; }
 
+                // Atualização dos valores de próximo e anterior para os nós adjacentes
                 if(node->prev != NULL) { node->prev->next = next; }
                 if(next->next != NULL) { next->next->prev = node; }
 
+                // Definição dos valores de próximo e anterior para os nós trocados
                 node_t *prev = node->prev;
                 node->next = next->next;
                 node->prev = next;
                 next->next = node;
                 next->prev = prev;
-                s++;
+
+                s++; // Flag que simboliza que houve alguma alteração
             }
             node = node->next;
         }
     }
 }
 
+// Função para realizar a impressão da lista
 void list_print(list_t *list) {
     int i = 0;
     node_t *node = list->head;
@@ -95,7 +116,6 @@ void list_print(list_t *list) {
 
 int main(void) {
     list_t *list = list_create();
-
     list_insert(list, 3, "bill");
     list_insert(list, 4, "neil");
     list_insert(list, 2, "john");
@@ -103,11 +123,10 @@ int main(void) {
     list_insert(list, 1, "alex");
     
     list_print(list);
-
-    printf("\n\n");
     list_sort(list);
-
+    printf("\n\n");
     list_print(list);
 
     list_destroy(list);
+    return;
 }
